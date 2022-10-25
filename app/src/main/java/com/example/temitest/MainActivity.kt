b18cae9d5bd4666849f638ed0cc548293d9adc13
DerLurchi <77876977+DerLurchi@users.noreply.github.com>
2022-10-25 16:56:08 +0200

    sdk eingebunden testfunktion eingebaut, die den roboter beim start der app an die home base schicken sollte

--- app/src/main/java/com/example/temitest/MainActivity.kt
+++ app/src/main/java/com/example/temitest/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.temitest
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,45 +19,44 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.temitest.ui.theme.TemiTestTheme
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.material.Button
 import androidx.compose.ui.res.painterResource
 import com.robotemi.sdk.Robot
 
-//import com.robotemi.sdk.*
-//import com.robotemi.sdk.Robot.*
+import com.robotemi.sdk.*
+import com.robotemi.sdk.Robot.*
 import com.robotemi.sdk.Robot.Companion.getInstance
 import com.robotemi.sdk.navigation.model.SpeedLevel
 
-//import com.robotemi.sdk.TtsRequest.Companion.create
-//import com.robotemi.sdk.activitystream.ActivityStreamObject
-//import com.robotemi.sdk.activitystream.ActivityStreamPublishMessage
-//import com.robotemi.sdk.constants.*
-//import com.robotemi.sdk.exception.OnSdkExceptionListener
-//import com.robotemi.sdk.exception.SdkException
-//import com.robotemi.sdk.face.ContactModel
-//import com.robotemi.sdk.face.OnContinuousFaceRecognizedListener
-//import com.robotemi.sdk.face.OnFaceRecognizedListener
-//import com.robotemi.sdk.listeners.*
-//import com.robotemi.sdk.map.Floor
-//import com.robotemi.sdk.map.MapModel
-//import com.robotemi.sdk.map.OnLoadFloorStatusChangedListener
-//import com.robotemi.sdk.map.OnLoadMapStatusChangedListener
-//import com.robotemi.sdk.model.CallEventModel
-//import com.robotemi.sdk.model.DetectionData
-//import com.robotemi.sdk.navigation.listener.OnCurrentPositionChangedListener
-//import com.robotemi.sdk.navigation.listener.OnDistanceToDestinationChangedListener
-//import com.robotemi.sdk.navigation.listener.OnDistanceToLocationChangedListener
-//import com.robotemi.sdk.navigation.listener.OnReposeStatusChangedListener
-//import com.robotemi.sdk.navigation.model.Position
-//import com.robotemi.sdk.navigation.model.SafetyLevel
-//import com.robotemi.sdk.navigation.model.SpeedLevel
-//import com.robotemi.sdk.permission.OnRequestPermissionResultListener
-//import com.robotemi.sdk.permission.Permission
-//import com.robotemi.sdk.sequence.OnSequencePlayStatusChangedListener
-//import com.robotemi.sdk.sequence.SequenceModel
-//import com.robotemi.sdk.telepresence.CallState
-//import com.robotemi.sdk.voice.ITtsService
-//import com.robotemi.sdk.voice.model.TtsVoice
+import com.robotemi.sdk.TtsRequest.Companion.create
+import com.robotemi.sdk.activitystream.ActivityStreamObject
+import com.robotemi.sdk.activitystream.ActivityStreamPublishMessage
+import com.robotemi.sdk.constants.*
+import com.robotemi.sdk.exception.OnSdkExceptionListener
+import com.robotemi.sdk.exception.SdkException
+import com.robotemi.sdk.face.ContactModel
+import com.robotemi.sdk.face.OnContinuousFaceRecognizedListener
+import com.robotemi.sdk.face.OnFaceRecognizedListener
+import com.robotemi.sdk.listeners.*
+import com.robotemi.sdk.map.Floor
+import com.robotemi.sdk.map.MapModel
+import com.robotemi.sdk.map.OnLoadFloorStatusChangedListener
+import com.robotemi.sdk.map.OnLoadMapStatusChangedListener
+import com.robotemi.sdk.model.CallEventModel
+import com.robotemi.sdk.model.DetectionData
+import com.robotemi.sdk.navigation.listener.OnCurrentPositionChangedListener
+import com.robotemi.sdk.navigation.listener.OnDistanceToDestinationChangedListener
+import com.robotemi.sdk.navigation.listener.OnDistanceToLocationChangedListener
+import com.robotemi.sdk.navigation.listener.OnReposeStatusChangedListener
+import com.robotemi.sdk.navigation.model.Position
+import com.robotemi.sdk.navigation.model.SafetyLevel
+import com.robotemi.sdk.permission.OnRequestPermissionResultListener
+import com.robotemi.sdk.permission.Permission
+import com.robotemi.sdk.sequence.OnSequencePlayStatusChangedListener
+import com.robotemi.sdk.sequence.SequenceModel
+import com.robotemi.sdk.telepresence.CallState
+import com.robotemi.sdk.voice.ITtsService
+import com.robotemi.sdk.voice.model.TtsVoice
+
 
 class MainActivity : ComponentActivity() {
 
@@ -66,17 +67,19 @@ class MainActivity : ComponentActivity() {
 
         robot = getInstance()
 
-        robot.goTo(location = "home base", backwards = true, noBypass = false, speedLevel = SpeedLevel.HIGH)
-
         setContent {
             TemiTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Temi")
-                }
+                Navigation()
+                robot.goTo(location = "home base", backwards = true, noBypass = false, speedLevel = SpeedLevel.HIGH)
             }
+
         }
     }
+}
+
+// Function to generate a Toast
+private fun mToast(context: Context){
+    Toast.makeText(context, "This is a Sample Toast", Toast.LENGTH_LONG).show()
 }
 
 @Composable
@@ -91,14 +94,60 @@ fun Greeting(name: String) {
             contentScale = ContentScale.Crop
             )
 
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(50.dp),
+            ) {
+            Text(text = "Navigate")
+        }
+
         Text(text = "Hello, my name is $name!", modifier = Modifier.padding(24.dp))
     }
+}
+
+
+@Composable
+fun Navigation( ){
+    val wc_icon = painterResource(id = R.drawable.wc_icon)
+    val mContext = LocalContext.current
+
+    Column {
+        Button(
+            onClick = {
+                /*TODO*/
+            },
+            modifier = Modifier.height(60.dp)
+        ) {
+            Text(text = "Das ist ein Testbutton")
+        }
+
+        Button(
+            onClick = {
+                mToast(mContext)
+                 },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Transparent,
+                contentColor = Color.Red),
+            elevation = ButtonDefaults.elevation(
+                defaultElevation = 0.dp
+            )
+//            modifier = Modifier.height(60.dp)
+        ) {
+            Image(
+                painter = wc_icon,
+                contentDescription = null,
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(100.dp))
+        }
+    }
+
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     TemiTestTheme {
-        Greeting("Rafael")
+        Navigation( )
     }
 }
